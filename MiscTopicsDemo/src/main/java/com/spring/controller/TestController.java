67dea50bb17dafc6f90e4527dea6ac2570f4046f
Prashant Dhoke <prashant.dhoke@zensar.com>
2022-04-21 12:39:00 +0530

    new repo

---
+++ MiscTopicsDemo/src/main/java/com/spring/controller/TestController.java
@@ -0,0 +1,33 @@
+package com.spring.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.spring.dto.DBConfig;
+
+@RestController
+public class TestController {
+
+	@Value("${spring.application.name}")
+	String appName;
+	
+
+	
+	@Autowired
+	DBConfig dbConfig;
+	
+	
+	
+	@GetMapping(value = "/dbconfig")
+	public String getDbConfig() {
+		return this.dbConfig.toString();
+		
+	}
+	@GetMapping(value = "/appName")
+	public String getAppName() {
+		return this.appName;
+	}
+
+}
